ec954f14465ca5ad552035d4f2cb15a093d89ab2
Julien Kauffmann <julien.kauffmann@freelan.org>
2022-01-09 16:31:11 -0500

    Better scrolling animation for header

--- static/js/nav.js
+++ static/js/nav.js
@@ -14,6 +14,10 @@ function getHeaderHeight() {
         * parseInt(getComputedStyle(document.body).getPropertyValue('--header-height'));
 }
 
+function isTouchDevice() {
+    return window.ontouchstart !== undefined;
+}
+
 function setupScroll() {
     const scrollUp = "scroll-up";
     const scrollDown = "scroll-down";
@@ -23,24 +27,36 @@ function setupScroll() {
 
     window.addEventListener("scroll", () => {
         const currentScroll = window.pageYOffset;
+        const isScrollUp = currentScroll < lastScroll;
+        const isScrollDown = currentScroll > lastScroll;
 
         if (currentScroll <= 0) {
             document.body.classList.remove(scrollUp);
             return;
         }
 
-        if (currentScroll > lastScroll && !document.body.classList.contains(scrollDown)) {
-            document.body.classList.remove(scrollUp);
-            document.body.classList.add(scrollDown);
-        } else if (
-            currentScroll < lastScroll &&
-            document.body.classList.contains(scrollDown)
-        ) {
-            document.body.classList.remove(scrollDown);
-            document.body.classList.add(scrollUp);
+        if (isScrollDown) {
+            if (!document.body.classList.contains(scrollDown)) {
+                document.body.classList.remove(scrollUp);
+                document.body.classList.add(scrollDown);
+            }
+        } else if (isScrollUp) {
+            if (document.body.classList.contains(scrollDown)) {
+                document.body.classList.remove(scrollDown);
+                document.body.classList.add(scrollUp);
+            }
+
+            if (isTouchDevice()) {
+                if (currentScroll < getHeaderHeight()) {
+                    window.scrollTo({
+                        top: 0,
+                        behavior: 'smooth'
+                    })
+                }
+            }
         }
 
-        if (currentScroll >= getHeaderHeight()) {
+        if (currentScroll >= 16) {
             document.body.classList.add(scrollPastHeader);
         } else {
             document.body.classList.remove(scrollPastHeader);
